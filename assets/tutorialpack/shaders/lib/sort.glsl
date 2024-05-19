ebebc996830a12ddc2de376ce7bb0695a97d67d6
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-05-19 17:50:31 -0400

    more algorithms

--- assets/tutorialpack/shaders/lib/sort.glsl
+++ assets/tutorialpack/shaders/lib/sort.glsl
@@ -3,33 +3,81 @@ struct Pair {
     float depth;
 };
 
-void swap6(Pair arr[6], int i, int j) {
-    if(arr[i].depth > arr[j].depth) {
+//NOTE: all algorithms must sort in descending order
+
+void swap(int i, int j, inout Pair arr[6]) {
+    if(arr[i].depth < arr[j].depth) {
         Pair temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 }
 
-void batcherSort6(Pair arr[6]) {
-    // First stage of comparison and swapping
-    swap6(arr, 0, 1);
-    swap6(arr, 2, 3);
-    swap6(arr, 4, 5);
+// Batcher's odd-even merge sort
+void batcherSort6(inout Pair arr[6]) {
+    swap(0, 1, arr);
+    swap(2, 3, arr);
+    swap(4, 5, arr);
+    swap(0, 2, arr);
+    swap(1, 3, arr);
+    swap(0, 4, arr);
+    swap(1, 5, arr);
+    swap(2, 4, arr);
+    swap(3, 5, arr);
+    swap(1, 2, arr);
+    swap(3, 4, arr);
+    swap(1, 4, arr);
+    swap(3, 2, arr);
+    swap(2, 4, arr);
+    swap(1, 3, arr);
+}
 
-    // Second stage of comparison and swapping
-    swap6(arr, 0, 2);
-    swap6(arr, 1, 3);
-    swap6(arr, 4, 2);
+// Insertion sort
+void insertionSort6(inout Pair arr[6]) {
+    for(int i = 1; i < 6; i++) {
+        Pair key = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j].depth < key.depth) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
 
-    // Third stage of comparison and swapping
-    swap6(arr, 1, 4);
-    swap6(arr, 3, 5);
+// Selection sort
+void selectionSort6(inout Pair arr[6]) {
+    for(int i = 0; i < 6; i++) {
+        int max = i;
+        for(int j = i + 1; j < 6; j++) {
+            if(arr[j].depth > arr[max].depth) {
+                max = j;
+            }
+        }
+        if(max != i) {
+            Pair temp = arr[i];
+            arr[i] = arr[max];
+            arr[max] = temp;
+        }
+    }
+}
 
-    // Fourth stage of comparison and swapping
-    swap6(arr, 1, 2);
-    swap6(arr, 3, 4);
+// Bubble sort
+void bubbleSort6(inout Pair arr[6]) {
+    for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 6 - i - 1; j++) {
+            if(arr[j].depth < arr[j + 1].depth) {
+                Pair temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
-    // Fifth stage of comparison and swapping
-    swap6(arr, 2, 3);
+void sort(inout Pair arr[6]) {
+//    insertionSort6(arr);
+//    selectionSort6(arr);
+//    bubbleSort6(arr);
+    batcherSort6(arr);
 }
--- assets/tutorialpack/shaders/post/composite.frag
+++ assets/tutorialpack/shaders/post/composite.frag
@@ -24,22 +24,20 @@ vec3 blend_colors(vec3 destination, vec4 source) {
 }
 
 void main() {
-    Pair main = Pair(texture(u_main_color, texcoord), texture(u_main_depth, texcoord).r);
-    Pair translucent = Pair(texture(u_translucent_color, texcoord), texture(u_translucent_depth, texcoord).r);
-    Pair entity = Pair(texture(u_entity_color, texcoord), texture(u_entity_depth, texcoord).r);
-    Pair weather = Pair(texture(u_weather_color, texcoord), texture(u_weather_depth, texcoord).r);
-    Pair clouds = Pair(texture(u_clouds_color, texcoord), texture(u_clouds_depth, texcoord).r);
-    Pair particles = Pair(texture(u_particles_color, texcoord), texture(u_particles_depth, texcoord).r);
-
     // Sort the pairs by depth
     Pair pairs[6];
-    pairs[0] = main; pairs[1] = translucent; pairs[2] = entity; pairs[3] = weather; pairs[4] = clouds; pairs[5] = particles;
-    batcherSort6(pairs);
+    pairs[0] = Pair(texture(u_main_color, texcoord), texture(u_main_depth, texcoord).r);
+    pairs[1] = Pair(texture(u_translucent_color, texcoord), texture(u_translucent_depth, texcoord).r);
+    pairs[2] = Pair(texture(u_entity_color, texcoord), texture(u_entity_depth, texcoord).r);
+    pairs[3] = Pair(texture(u_weather_color, texcoord), texture(u_weather_depth, texcoord).r);
+    pairs[4] = Pair(texture(u_clouds_color, texcoord), texture(u_clouds_depth, texcoord).r);
+    pairs[5] = Pair(texture(u_particles_color, texcoord), texture(u_particles_depth, texcoord).r);
+
+    sort(pairs);
 
     vec3 composite = vec3(0.0);
     for (int i = 0; i < 6; i++) {
-        Pair pair = pairs[i];
-        composite = blend_colors(composite, pair.color);
+        composite = blend_colors(composite, pairs[i].color);
     }
 
     // Alpha is mostly ignored, but we will set it to one
